7ef266eeefa17d7f5a2a1efc2c41f4998013d5d0
srinivaschalla-source <srinivas10199@gmail.com>
2025-12-24 12:43:39 +0530

    This is my first commit

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+a=input("enter first number: ")
+a=int(a)
+b=input("enter second number: ")
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition: ", c)
+print("Subtraction: ",d)
+print("Multiplication: ",e)
+print("Division: ", f)
+
