13b9914c4c277c8f1f5a97df798c86cf875e6333
srinivaschalla-source <srinivas10199@gmail.com>
2026-02-22 13:41:38 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,8 +1,9 @@
 #Programme for taking input of an integer and finding and printing whether  it is even or odd number in the given format.
-#Taking input of an integer in a variable x
+# step 1 Taking input of an integer in a variable x---Takes an integer input from the user.
 x=int(input("Enter an integer: "))
-# conditional verification using MOD operator if the entered value is divisible by 2 or not and if remainder is 0, printing it as even number else as odd number
+# step 2 -Checks whether the number is even or odd using an if-else statement.Conditional verification using MOD operator if the entered value is divisible by 2 or not and if remainder is 0, printing it as even number else as odd number
 if x%2==0:
+# step 3---Displays the result accordingly.
     print(f"{x} is an Even number")
 else:
     print(f"{x} is an Odd number")
