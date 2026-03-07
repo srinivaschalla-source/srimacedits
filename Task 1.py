a03b407713de28b8360db651fc451d1eed90e579
srinivaschalla-source <srinivas10199@gmail.com>
2026-03-07 09:47:38 +0530

    Assignment 5

--- Task 1.py
+++ Task 1.py
@@ -1,25 +1,12 @@
-"""
-# This block is for creating a file named sampl.txt and writing two lines into it in write mode"
-#creating a file named sample.txt in w mode for creating a text file
-with open('sample.txt', 'w') as file_handler:
-#writing 2 lines into the text file
-    file_handler.write("This is a sample text file \n") 
-    file_handler.write("It contains multiple lines")
-"""
-# using infinite loop function to open file in read mode and print lines one by one if file exists
-file_name="sample.txt"
-line_count = 0
-with open('sample.txt', 'r') as f:
-    for line in f:
-        line_count += 1
-while True:
-    try:
-        with open('sample.txt','r') as file_handler:
-            for i in range(line_count):
-                content=file_handler.readline()
-                print(content)
-        break
- # exception block for handling error       
-    except FileNotFoundError:
-        print(f"The file {file_name} was not found")
-        break
+# This is a program for creating a Dictionary of Student marks and retrieve the marks based on their name from the dictionary.
+# If students name is not found, giving an appropriate message.
+
+# Creating a dictionary with 4 students names and marks.
+dictionary_students={"Alpha":40,"Beta":70,"Gamma":90,"Theta":99}
+#taking input of the student name
+student_name = input("Enter the student's name: ")
+#checking for details of the student in dictionary and printing corresponding marks.
+if student_name in dictionary_students:
+    print(f"{student_name} marks: {dictionary_students[student_name]}")
+else:
+    print("Student not found. ")
