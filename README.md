adceb7167d5349ae01fd55488a6bc91881c71801
srinivaschalla-source <srinivas10199@gmail.com>
2026-03-07 09:49:06 +0530

    Assignment 5

--- README.md
+++ README.md
@@ -1,23 +1,6 @@
-ASSIGNMENT 5:
 
-Task 1:
-
-Project title:
-----------------
-Create a Dictionary of Student Marks and retrieve marks by calling students name.
-
-Description:
--------------
-TO create a dictionary of students with marks. Taking input of the student name and searching dictionary for name and fetching marks if name is present else giving a message that student details not found. Here matching the keys and values of the dictionary and printing the values of the keys is key concept. 
+Assignments
 
 
-Task 2:
-
-Project title:
-----------------
-Demonstrate List Slicing 
 
 
-Description:
--------------
-The program creates a list of numbers from 1 to 10. It extracts the first 5 elements of the list into a seperate list and prints the same. It Reverses the extracted list using reverse function and changes the extracted list with reversed list and prints the same. The concepts like list slicing, reverse function, printing of the list using indexing are understood. 
--- Task 2.py
+++ Task 2.py
@@ -1,18 +1,10 @@
-
-#Taking user input and writing it to a file named Output.txt
-Data=input("Enter text to write to the file : ")
-#writing a file with input text in w mode.
-with open('output.txt','w') as fh:
-    fh.write(Data)
-print("Data successfully written to the output.txt file.")
-#taking additional data as input and appending the same to text file.
-Data1=input("Enter additional text to append: ")
-with open('output.txt','a') as fh:
-    fh.write("\n" + Data1)
-print("Data successfully appended. ")
-#opening the file in read mode and using readlines function printing the output of the file line by line.
-with open('output.txt','r') as fh:
-    Data3=fh.readlines()
-print("Final content of the output.txt")
-for line in Data3:
-    print(line.strip())
+#This program is for creating a list with numbers 1 to 10 as elements. Extract first five elements and then reverse the extracted elements and print both the extracted list and reversed list.
+#step 1: creating a list with 10 numbers and printing it
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print(f"Original list: {numbers}")
+#step2: extracting the first 5 elements of the list into a seperate list and printing the same.
+extracted_list=numbers[0:5]
+print(f"extracted first five elements: {extracted_list}")
+#step 3: Reversing the extracted list using reverse function and printing the same list.
+reversed_list=extracted_list.reverse()
+print(f"Reversed extracted elements: {extracted_list}")
